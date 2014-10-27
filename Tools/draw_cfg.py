04e4c092e340bfeb8a6b05c0460660af722393f7
Edward Laird <elaird@gmail.com>
2014-10-27 05:14:32 -0500

    change location; comment non-tt

--- Tools/draw_cfg.py
+++ Tools/draw_cfg.py
@@ -2,22 +2,23 @@ import ROOT as r
 
 
 drawConfigs = {'script': 'drawVarsData_KS_CheckFlat.py',
-               'sampleLocation': '/scratch/zmao/relaxed_regression4',
+               #'sampleLocation': '/scratch/zmao/relaxed_regression4/TMVARegApp_',
+               'sampleLocation': '/nfs_scratch/elaird/zm-old/',
                'signal': 'H260'}
 
 SamplePreFix = drawConfigs['sampleLocation']
-SamplePreFix = SamplePreFix + '/TMVARegApp_'
-MCFileList = [('ZZ', SamplePreFix + 'ZZ_eff_all.root', 2500, 5),
-            ('WZJetsTo2L2Q', SamplePreFix + 'WZJetsTo2L2Q_eff_all.root', 2207, 5),
-            ('W1JetsToLNu', SamplePreFix + 'W1JetsToLNu_eff2_all.root', 5400000, r.kMagenta-9),
-            ('W2JetsToLNu', SamplePreFix + 'W2JetsToLNu_eff2_all.root', 1750000, r.kMagenta-9),
-            ('W3JetsToLNu', SamplePreFix + 'W3JetsToLNu_eff2_all.root', 519000, r.kMagenta-9),
-            #('DYJetsToLL', 'TMVARegApp_DYJetsToLL_eff_all.root', 3504000, r.kGreen-7),
-            ('DY1JetsToLL', SamplePreFix + 'DY1JetsToLL_eff2_all.root', 561000, r.kGreen-7),
-            ('DY2JetsToLL', SamplePreFix + 'DY2JetsToLL_eff2_all.root', 181000, r.kGreen-7),
-            ('DY3JetsToLL', SamplePreFix + 'DY3JetsToLL_eff2_all.root', 51100, r.kGreen-7),
-            ('tt_full_lep',SamplePreFix + 'tt_eff_all.root', 26197.5, r.kRed-7),
-            ('tt_semi_lep',SamplePreFix + 'tt_semi_eff_all.root', 109281, r.kAzure+7),]
+
+MCFileList = [#('ZZ', SamplePreFix + 'ZZ_eff_all.root', 2500, 5),
+              #('WZJetsTo2L2Q', SamplePreFix + 'WZJetsTo2L2Q_eff_all.root', 2207, 5),
+              #('W1JetsToLNu', SamplePreFix + 'W1JetsToLNu_eff2_all.root', 5400000, r.kMagenta-9),
+              #('W2JetsToLNu', SamplePreFix + 'W2JetsToLNu_eff2_all.root', 1750000, r.kMagenta-9),
+              #('W3JetsToLNu', SamplePreFix + 'W3JetsToLNu_eff2_all.root', 519000, r.kMagenta-9),
+              ##('DYJetsToLL', 'TMVARegApp_DYJetsToLL_eff_all.root', 3504000, r.kGreen-7),
+              #('DY1JetsToLL', SamplePreFix + 'DY1JetsToLL_eff2_all.root', 561000, r.kGreen-7),
+              #('DY2JetsToLL', SamplePreFix + 'DY2JetsToLL_eff2_all.root', 181000, r.kGreen-7),
+              #('DY3JetsToLL', SamplePreFix + 'DY3JetsToLL_eff2_all.root', 51100, r.kGreen-7),
+              ('tt_full_lep',SamplePreFix + 'tt_eff_all.root', 26197.5, r.kRed-7),
+              ('tt_semi_lep',SamplePreFix + 'tt_semi_eff_all.root', 109281, r.kAzure+7),]
 
 singalPreFix = SamplePreFix
 signalDict = {'H260': (singalPreFix + 'H2hh260_all.root', 14.76),
@@ -52,7 +53,7 @@ varsRange = {
 #             'J4CSVbtag':(12, 0, 1.2, 50000, 100, True, True),
 #             'J4Eta': (20, -3.14, 3.14, 10000, 100, True, True),
 #             'J4Pt': (15, 0, 250, 5000, 100, True, True),
-#             'svMass': (10, 0, 400, 20, 100, False, True),
+#             'svMass': (10, 0, 400, 40, 100, False, True),
 #             'svPt': (10, 0, 600, 50, 100, False, True),
 #              'mJJ': (15, 80, 150, 30000, 100, True, True),
 #              'mJJReg': (10, 50, 200, 20, 100, False, True),
