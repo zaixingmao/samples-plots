1122c9eefa52420797d9cb12f6d118a83e9f5ea6
Zaixing <zaixing.mao@cern.ch>
2014-11-14 04:02:08 -0600

    config changes

--- makeWholeSample_cfg.py
+++ makeWholeSample_cfg.py
@@ -8,16 +8,26 @@
 
 #preFix0 = '/scratch/zmao/relaxed_regression4/'
 # preFix0 = '/scratch/zmao/newKinFit/8/'
-preFix0 = '/scratch/zmao/jetPt20/'
+# preFix0 = '/scratch/zmao/jetPt20/'
+preFix0 = '/scratch/zmao/v3/'
+
 # preFix0 = '/scratch/zmao/BDTStudy/7_noDPhiMetJ2_mJJ/'
 
 
 postFix = ''
 bTag = '1M'
 
+thirdLeptonVeto = False
+
 scaleFactors = {'bTag': 0.051,
-                '2M': 0.069,
-                '1M': 0.053}  
+                '2M': 0.058,
+                '1M': 0.0523}  
+
+if thirdLeptonVeto:
+    scaleFactors = {'bTag': 0.051,
+                    '2M': 0.0507,
+                    '1M': 0.0498}
+
 
 sampleConfigs =[('H2hh260', 'H2hh260_all%s.root' %postFix, 'OStight%s' %bTag, 14.76),
 
@@ -34,13 +44,14 @@ sampleConfigs =[('H2hh260', 'H2hh260_all%s.root' %postFix, 'OStight%s' %bTag, 14
 
                 ('H2hh350', 'H2hh350_all%s.root' %postFix, 'OStight%s' %bTag, 8.57),
 
-#                 ('H2hh500', 'H2hh500_all.root', 'OStight%s' %bTag, 8.57),
-#                 ('H2hh700', 'H2hh700_all.root', 'OStight%s' %bTag, 8.57),
+                ('H2hh500', 'H2hh500_all.root', 'OStight%s' %bTag, 8.57),
+                ('H2hh700', 'H2hh700_all.root', 'OStight%s' %bTag, 8.57),
+                ('H2hh1000', 'H2hh1000_all.root', 'OStight%s' %bTag, 8.57),
 
-                ('ZZ','ZZ_eff_all%s.root' %postFix, 'OStight%s' %bTag, 2500),
-                ("tt_full","tt_eff_all%s.root" %postFix, 'OStight%s' %bTag, 26197.5),
-                ("tt_semi","tt_semi_eff_all%s.root" %postFix, 'OStight%s' %bTag, 109281),
-                #('WZJetsTo2L2Q', 'WZJetsTo2L2Q_eff_all.root', 'OStight%s' %bTag, 2207),
+                ('ZZ','ZZ_all%s.root' %postFix, 'OStight%s' %bTag, 2500),
+                ("tt_full","tt_all%s.root" %postFix, 'OStight%s' %bTag, 26197.5),
+                ("tt_semi","tt_semi_all%s.root" %postFix, 'OStight%s' %bTag, 109281),
+                ('WZJetsTo2L2Q', 'WZJetsTo2L2Q_all.root', 'OStight%s' %bTag, 2207),
                 ("DY1JetsToLL","DY1JetsToLL_all.root", 'OStight%s' %bTag, 561000),
                 ('DY2JetsToLL','DY2JetsToLL_all.root', 'OStight%s' %bTag, 181000),
                 ('DY3JetsToLL','DY3JetsToLL_all.root', 'OStight%s' %bTag, 51100),
@@ -49,5 +60,5 @@ sampleConfigs =[('H2hh260', 'H2hh260_all%s.root' %postFix, 'OStight%s' %bTag, 14
                 ('W3JetsToLNu','W3JetsToLNu_all.root', 'OStight%s' %bTag, 519000),
                 ('dataOSRelax','dataTotal_all%s.root' %postFix, 'OSrelaxed%s' %bTag, scaleFactors[bTag])]
 
-oFileName = 'combined_all%s_%s.root' %(postFix, bTag)
+oFileName = 'combined%s_%s.root' %(postFix, bTag)
 trainedMassPoints = [260, 270, 280, 290, 300, 310, 320, 330, 340, 350]
