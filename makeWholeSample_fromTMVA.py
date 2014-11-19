a4179f0b1c53aa87897d363f49da7940b6549640
Zaixing <zaixing.mao@cern.ch>
2014-11-19 03:13:16 -0600

    config changesvarsList.py

---
+++ makeWholeSample_fromTMVA.py
@@ -0,0 +1,295 @@
+#!/usr/bin/env python
+import ROOT as r
+from operator import itemgetter
+import tool
+import math
+import optparse
+import os
+from array import array
+import makeWholeSample_cfg
+import makeWholeTools
+
+lvClass = r.Math.LorentzVector(r.Math.PtEtaPhiM4D('double'))
+
+
+lumi = 19.7
+
+
+
+
+def makeWhole(iFileName, iLocation, weight0, weight1, weight2, sample):
+    ifile = r.TFile(iLocation)
+    iTestTree = ifile.Get('TestTree')
+    iTrainTree = ifile.Get('TrainTree')
+    sampleNames = []
+    xsValues = {}
+    initEvents = {}
+    sampleNamesCounterTest_1 = {}
+    sampleNamesCounterTest_2 = {}
+    sampleNamesCounter_triggerEff1 = {}
+    sampleNamesCounter_triggerEff2 = {}
+    sampleNamesCounterTrain_1 = {}
+    sampleNamesCounterTrain_2 = {}
+    dataCounter1 = 0
+    dataCounter2 = 0
+    categories = []
+    sampleName = bytearray(30)
+    category2 = bytearray(30)
+
+    validDict1 = {}
+    validDict2 = {}
+
+    mJJReg = array('f', [0.])
+    mJJ = array('f', [0.])
+    CSVJ2 = array('f', [0.])
+    BDT = array('f', [0.])
+    met = array('f', [0.])
+    fMass = array('f', [0.])
+    fMassKinFit = array('f', [0.])
+    chi2KinFit = array('f', [0.])
+    chi2KinFit2 = array('f', [0.])
+    svMass = array('f', [0.])
+    CSVJ2 = array('f', [0.])
+    triggerEff = array('f', [0.])
+
+    totalTest = iTestTree.GetEntries()
+    totalTrain = iTrainTree.GetEntries()
+    fracs1 = {}
+    fracs2 = {}
+
+    L_to_T_1M = r.TH1F('L_to_T_1M', 'L_to_T_1M', 1, 0, 1)
+    L_to_T_2M = r.TH1F('L_to_T_2M', 'L_to_T_2M', 1, 0, 1)
+    L_to_T_1M.Fill(0.5, weight1)
+    L_to_T_2M.Fill(0.5, weight2)
+
+    oFile = r.TFile('combined_%s.root' %iFileName, 'recreate')
+    oTree = r.TTree('eventTree', '')
+
+    oTree.Branch("sampleName", sampleName, "sampleName[31]/C")
+    oTree.Branch("Category", category2, "Category[31]/C")
+
+    oTree.Branch("mJJReg", mJJReg, "mJJReg/F")
+    oTree.Branch("mJJ", mJJ, "mJJ/F")
+    oTree.Branch("fMass", fMass, "fMass/F")
+    oTree.Branch("fMassKinFit", fMassKinFit, "fMassKinFit/F")
+    oTree.Branch("chi2KinFit", chi2KinFit, "chi2KinFit/F")
+    oTree.Branch("chi2KinFit2", chi2KinFit2, "chi2KinFit2/F")
+    oTree.Branch("svMass", svMass, "svMass/F")
+    oTree.Branch("met", met, "met/F")
+    oTree.Branch("BDT", BDT, "BDT/F")
+
+    oTree.Branch("CSVJ2", CSVJ2, "CSVJ2/F")
+
+    oTree.Branch("triggerEff", triggerEff, "triggerEff/F")
+
+    for i in range(totalTrain):
+        tool.printProcessStatus(iCurrent=i+1, total=totalTest, processName = 'Looping sample [%s]' %iFileName)
+        iTrainTree.GetEntry(i)
+        tmpSampleName = tool.nameEnDecoder(iTrainTree.sampleName2, 'decode')
+        if tmpSampleName not in sampleNames:
+            sampleNames.append(tmpSampleName)
+            sampleNamesCounterTest_1[tmpSampleName] = 0.0
+            sampleNamesCounterTest_2[tmpSampleName] = 0.0
+            sampleNamesCounterTrain_1[tmpSampleName] = 0.0
+            sampleNamesCounterTrain_2[tmpSampleName] = 0.0
+            sampleNamesCounter_triggerEff1[tmpSampleName] = 0.0
+            sampleNamesCounter_triggerEff2[tmpSampleName] = 0.0
+            if iTrainTree.category == 1:
+                sampleNamesCounterTrain_1[tmpSampleName] += 1.0
+            elif iTrainTree.category == 2:
+                sampleNamesCounterTrain_2[tmpSampleName] += 1.0
+            initEvents[tmpSampleName] = iTrainTree.initEvents
+            xsValues[tmpSampleName] = iTrainTree.xs
+        else:
+            if iTrainTree.category == 1:
+                sampleNamesCounterTrain_1[tmpSampleName] += 1.0
+
+            elif iTrainTree.category == 2:
+                sampleNamesCounterTrain_2[tmpSampleName] += 1.0
+        sampleName[:31] = tmpSampleName
+        if tmpSampleName == 'tt':
+            sampleName[:31] = 'tt_full'
+        if tmpSampleName == 'data':
+            sampleName[:31] = 'dataOSRelax'
+        if iTrainTree.category == 1:
+            category2[:31] = '1M'
+        elif iTrainTree.category == 2:
+            category2[:31] = '2M'
+        else:
+            category2[:31] = 'None'
+        mJJReg[0] = iTrainTree.mJJReg
+        mJJ[0] = iTrainTree.mJJ
+        CSVJ2[0] = iTrainTree.CSVJ2
+        fMass[0] = iTrainTree.fMass
+        fMassKinFit[0] = iTrainTree.fMassKinFit
+        chi2KinFit[0] = iTrainTree.chi2KinFit
+        chi2KinFit2[0] = iTrainTree.chi2KinFit2
+        svMass[0] = iTrainTree.svMass
+        triggerEff[0] = iTrainTree.triggerEff
+        BDT[0] = iTrainTree.BDT
+
+        if 'train' in sample:
+            if iTrainTree.category == 1:
+                sampleNamesCounter_triggerEff1[tmpSampleName] += iTrainTree.triggerEff
+            elif iTrainTree.category == 2:
+                sampleNamesCounter_triggerEff2[tmpSampleName] += iTrainTree.triggerEff
+            oTree.Fill()         
+
+
+    for i in range(totalTest):
+        tool.printProcessStatus(iCurrent=i+1, total=totalTest, processName = 'Looping sample [%s]' %iFileName)
+        iTestTree.GetEntry(i)
+        tmpSampleName = tool.nameEnDecoder(iTestTree.sampleName2, 'decode')
+        if tmpSampleName not in sampleNames:
+            sampleNames.append(tmpSampleName)
+            sampleNamesCounterTest_1[tmpSampleName] = 0.0
+            sampleNamesCounterTest_2[tmpSampleName] = 0.0
+            sampleNamesCounter_triggerEff1[tmpSampleName] = 0.0
+            sampleNamesCounter_triggerEff2[tmpSampleName] = 0.0
+            sampleNamesCounterTrain_1[tmpSampleName] = 0.0
+            sampleNamesCounterTrain_2[tmpSampleName] = 0.0
+            if iTestTree.category == 1:
+                sampleNamesCounterTrain_1[tmpSampleName] += 1.0
+            elif iTestTree.category == 2:
+                sampleNamesCounterTrain_2[tmpSampleName] += 1.0
+            initEvents[tmpSampleName] = iTestTree.initEvents
+            xsValues[tmpSampleName] = iTestTree.xs
+
+        else:
+            if iTestTree.category == 1:
+                sampleNamesCounterTest_1[tmpSampleName] += 1.0
+            elif iTestTree.category == 2:
+                sampleNamesCounterTest_2[tmpSampleName] += 1.0
+
+        sampleName[:31] = tmpSampleName
+        if tmpSampleName == 'tt':
+            sampleName[:31] = 'tt_full'
+        if tmpSampleName == 'data':
+            sampleName[:31] = 'dataOSRelax'
+        if iTestTree.category == 1:
+            category2[:31] = '1M'
+        elif iTestTree.category == 2:
+            category2[:31] = '2M'
+        else:
+            category2[:31] = 'None'
+        mJJReg[0] = iTestTree.mJJReg
+        BDT[0] = iTestTree.BDT
+
+        mJJ[0] = iTestTree.mJJ
+        CSVJ2[0] = iTestTree.CSVJ2
+        fMass[0] = iTestTree.fMass
+        fMassKinFit[0] = iTestTree.fMassKinFit
+        chi2KinFit[0] = iTestTree.chi2KinFit
+        chi2KinFit2[0] = iTestTree.chi2KinFit2
+        met[0] = iTestTree.met
+
+        svMass[0] = iTestTree.svMass
+        triggerEff[0] = iTestTree.triggerEff
+        if 'test' in sample:
+            if iTestTree.category == 1:
+                sampleNamesCounter_triggerEff1[tmpSampleName] += iTestTree.triggerEff
+            elif iTestTree.category == 2:
+                sampleNamesCounter_triggerEff2[tmpSampleName] += iTestTree.triggerEff
+            oTree.Fill()
+
+    print ''
+
+    initEventsHist = r.TH1F('initEvents', 'initEvents', len(sampleNames), 0, len(sampleNames))
+    initEvents_1tag = r.TH1F('initEvents_1tag', 'initEvents_1tag', len(sampleNames), 0, len(sampleNames))
+    initEvents_2tag = r.TH1F('initEvents_2tag', 'initEvents_2tag', len(sampleNames), 0, len(sampleNames))
+
+    xs = r.TH1F('xs', 'xs', len(sampleNames), 0, len(sampleNames))
+    for iSample in sampleNames:
+        if (sampleNamesCounterTest_1[iSample]+sampleNamesCounterTrain_1[iSample]) != 0:
+            fracs1[iSample] = (sampleNamesCounterTest_1[iSample])/(sampleNamesCounterTest_1[iSample]+sampleNamesCounterTrain_1[iSample])
+        else:
+            fracs1[iSample] = 0
+        if (sampleNamesCounterTest_2[iSample]+sampleNamesCounterTrain_2[iSample]) != 0:
+           fracs2[iSample] = (sampleNamesCounterTest_2[iSample])/(sampleNamesCounterTest_2[iSample]+sampleNamesCounterTrain_2[iSample])
+        else:
+            fracs2[iSample] = 0
+        initEventsHist.Fill(iSample, initEvents[iSample])
+        if ('train' not in sample):
+            initEvents_1tag.Fill(iSample, initEvents[iSample]*fracs1[iSample])
+            initEvents_2tag.Fill(iSample, initEvents[iSample]*fracs2[iSample])
+
+        if 'data' in iSample:
+            xs.Fill(iSample, weight0)
+
+        xs.Fill(iSample, xsValues[iSample])
+        extraSpace = ''
+        if len(iSample) < 8:
+            extraSpace = '\t'
+
+        if 'tt' in iSample:
+            if 'tt' in validDict1.keys():
+                validDict1['tt'] += sampleNamesCounter_triggerEff1[iSample]/initEvents[iSample]*lumi*xsValues[iSample]*1000
+                validDict2['tt'] += sampleNamesCounter_triggerEff2[iSample]/initEvents[iSample]*lumi*xsValues[iSample]*1000
+
+            else:
+                validDict1['tt'] = sampleNamesCounter_triggerEff1[iSample]/initEvents[iSample]*lumi*xsValues[iSample]*1000
+                validDict2['tt'] = sampleNamesCounter_triggerEff2[iSample]/initEvents[iSample]*lumi*xsValues[iSample]*1000
+
+        elif 'DY' in iSample:
+            if 'DY' in validDict1.keys():
+                validDict1['DY'] += sampleNamesCounter_triggerEff1[iSample]/initEvents[iSample]*lumi*xsValues[iSample]*1000
+                validDict2['DY'] += sampleNamesCounter_triggerEff2[iSample]/initEvents[iSample]*lumi*xsValues[iSample]*1000
+
+            else:
+                validDict1['DY'] = sampleNamesCounter_triggerEff1[iSample]/initEvents[iSample]*lumi*xsValues[iSample]*1000
+                validDict2['DY'] = sampleNamesCounter_triggerEff2[iSample]/initEvents[iSample]*lumi*xsValues[iSample]*1000
+
+        elif 'Z' in iSample:
+            if 'Z' in validDict1.keys():
+                validDict1['Z'] += sampleNamesCounter_triggerEff1[iSample]/initEvents[iSample]*lumi*xsValues[iSample]*1000
+                validDict2['Z'] += sampleNamesCounter_triggerEff2[iSample]/initEvents[iSample]*lumi*xsValues[iSample]*1000
+
+            else:
+                validDict1['Z'] = sampleNamesCounter_triggerEff1[iSample]/initEvents[iSample]*lumi*xsValues[iSample]*1000
+                validDict2['Z'] = sampleNamesCounter_triggerEff2[iSample]/initEvents[iSample]*lumi*xsValues[iSample]*1000
+
+        elif 'LNu' in iSample:
+            if 'W' in validDict1.keys():
+                validDict1['W'] += sampleNamesCounter_triggerEff1[iSample]/initEvents[iSample]*lumi*xsValues[iSample]*1000
+                validDict2['W'] += sampleNamesCounter_triggerEff2[iSample]/initEvents[iSample]*lumi*xsValues[iSample]*1000
+
+            else:
+                validDict1['W'] = sampleNamesCounter_triggerEff1[iSample]/initEvents[iSample]*lumi*xsValues[iSample]*1000
+                validDict2['W'] = sampleNamesCounter_triggerEff2[iSample]/initEvents[iSample]*lumi*xsValues[iSample]*1000
+        elif 'data' in iSample:
+            validDict1['QCD'] = sampleNamesCounter_triggerEff1[iSample]*weight1
+            validDict2['QCD'] = sampleNamesCounter_triggerEff2[iSample]*weight2
+            
+        else:
+            validDict1[iSample] = sampleNamesCounter_triggerEff1[iSample]/initEvents[iSample]*lumi*xsValues[iSample]*1000
+            validDict2[iSample] = sampleNamesCounter_triggerEff2[iSample]/initEvents[iSample]*lumi*xsValues[iSample]*1000
+            
+
+    for iSample in validDict1.keys():
+        extraSpace = ''
+        if len(iSample) < 2:
+            extraSpace = '\t'
+        print "cat1: %s\t%s%0.4f" %(iSample,extraSpace, validDict1[iSample])
+    print ''
+    for iSample in validDict2.keys():
+        extraSpace = ''
+        if len(iSample) < 2:
+            extraSpace = '\t'
+        print "cat2: %s\t%s%0.4f" %(iSample,extraSpace, validDict2[iSample])
+
+
+    oFile.cd()
+    initEventsHist.Write()
+    xs.Write()
+    L_to_T_1M.Write()
+    L_to_T_2M.Write()
+    oTree.SetName('eventTree')
+    oTree.Write()
+    oFile.Close()
+
+weights = makeWholeTools.calculateSF(makeWholeSample_cfg.sampleConfigs, makeWholeSample_cfg.preFixTools, 'veto012')
+
+massPoints = ['260', '300', '350']
+postFix = '_8_n150_1M'
+for iMass in massPoints:
+    makeWhole('H%s%s' %(iMass,postFix), '/scratch/zmao/TMVA/new3/TMVA%s%s.root' %(iMass,postFix), weights[0], weights[1], weights[2], 'traintest')
---
+++ makeWholeTools.py
@@ -0,0 +1,223 @@
+#!/usr/bin/env python
+import ROOT as r
+from operator import itemgetter
+import tool
+import math
+import optparse
+import os
+from array import array
+import makeWholeSample_cfg
+
+lvClass = r.Math.LorentzVector(r.Math.PtEtaPhiM4D('double'))
+
+
+lumi = 19.7
+
+def passCut(tree, option):
+    passIso = 0
+    passSign = 0
+    if 'tight' in option and (tree.iso1.at(0) < 1.5 and tree.iso2.at(0) < 1.5):
+            passIso = 1
+    if 'relaxed' in option and (tree.iso1.at(0) > 3 and tree.iso2.at(0) > 3):
+            passIso = 1
+    if 'SS' in option and (tree.charge1.at(0) == tree.charge2.at(0)):
+            passSign = 1
+    if 'OS' in option and (tree.charge1.at(0) == -tree.charge2.at(0)):
+            passSign = 1
+    return passIso*passSign
+
+
+def calculateSF(fileList, location0, out, verbose = False):
+    files = []
+    trees = []
+
+    MC_SS_TT_cat1 = 0
+    MC_SS_LL_cat1 = 0
+    MC_OS_LL_cat1 = 0
+    DATA_SS_TT_cat1 = 0
+    DATA_SS_LL_cat1 = 0
+    DATA_OS_LL_cat1 = 0
+
+    MC_SS_TT_cat2 = 0
+    MC_SS_LL_cat2 = 0
+    MC_OS_LL_cat2 = 0
+    DATA_SS_TT_cat2 = 0
+    DATA_SS_LL_cat2 = 0
+    DATA_OS_LL_cat2 = 0
+
+    MC_SS_TT_cat1_veto = 0
+    MC_SS_LL_cat1_veto = 0
+    MC_OS_LL_cat1_veto = 0
+    DATA_SS_TT_cat1_veto = 0
+    DATA_SS_LL_cat1_veto = 0
+    DATA_OS_LL_cat1_veto = 0
+
+    MC_SS_TT_cat2_veto = 0
+    MC_SS_LL_cat2_veto = 0
+    MC_OS_LL_cat2_veto = 0
+    DATA_SS_TT_cat2_veto = 0
+    DATA_SS_LL_cat2_veto = 0
+    DATA_OS_LL_cat2_veto = 0
+
+    for fileName, location, selection, xs in fileList:
+
+        files.append(r.TFile(location0+location))
+        trees.append(files[len(files)-1].Get("eventTree"))
+        initEvents = (files[len(files)-1].Get("preselection")).GetBinContent(1)
+        total = trees[len(trees)-1].GetEntries()
+        if 'data' in fileName:
+            isData = True
+        else:
+            isData = False
+        if 'H2hh' in fileName:
+            continue
+        for i in range(total):
+            tool.printProcessStatus(iCurrent=i+1, total=total, processName = 'Looping sample [%s]' %fileName)
+            trees[len(trees)-1].GetEntry(i)
+            if trees[len(trees)-1].category == '1M1NonM':
+                if passCut(trees[len(trees)-1], 'tightSS'):
+                    if isData:
+                        DATA_SS_TT_cat1+=1
+                    else:
+                        MC_SS_TT_cat1+=xs*(lumi/initEvents)*trees[len(trees)-1].triggerEff
+                elif passCut(trees[len(trees)-1], 'relaxedOS'):
+                    if isData:
+                        DATA_OS_LL_cat1+=1
+                    else:
+                        MC_OS_LL_cat1+=xs*(lumi/initEvents)*trees[len(trees)-1].triggerEff
+                elif passCut(trees[len(trees)-1], 'relaxedSS'):
+                    if isData:
+                        DATA_SS_LL_cat1+=1
+                    else:
+                        MC_SS_LL_cat1 +=xs*(lumi/initEvents)*trees[len(trees)-1].triggerEff
+            elif trees[len(trees)-1].category == '2M':
+                if passCut(trees[len(trees)-1], 'tightSS'):
+                    if isData:
+                        DATA_SS_TT_cat2 += 1
+                    else:
+                        MC_SS_TT_cat2 += xs*(lumi/initEvents)*trees[len(trees)-1].triggerEff
+                elif passCut(trees[len(trees)-1], 'relaxedOS'):
+                    if isData:
+                        DATA_OS_LL_cat2 += 1
+                    else:
+                        MC_OS_LL_cat2 += xs*(lumi/initEvents)*trees[len(trees)-1].triggerEff
+                elif passCut(trees[len(trees)-1], 'relaxedSS'):
+                    if isData:
+                        DATA_SS_LL_cat2 += 1
+                    else:
+                        MC_SS_LL_cat2 += xs*(lumi/initEvents)*trees[len(trees)-1].triggerEff
+
+            if trees[len(trees)-1].nElectrons == 0 and trees[len(trees)-1].nMuons == 0:
+                if trees[len(trees)-1].category == '1M1NonM':
+                    if passCut(trees[len(trees)-1], 'tightSS'):
+                        if isData:
+                            DATA_SS_TT_cat1_veto+=1
+                        else:
+                            MC_SS_TT_cat1_veto+=xs*(lumi/initEvents)*trees[len(trees)-1].triggerEff
+                    elif passCut(trees[len(trees)-1], 'relaxedOS'):
+                        if isData:
+                            DATA_OS_LL_cat1_veto+=1
+                        else:
+                            MC_OS_LL_cat1_veto+=xs*(lumi/initEvents)*trees[len(trees)-1].triggerEff
+                    elif passCut(trees[len(trees)-1], 'relaxedSS'):
+                        if isData:
+                            DATA_SS_LL_cat1_veto+=1
+                        else:
+                            MC_SS_LL_cat1_veto +=xs*(lumi/initEvents)*trees[len(trees)-1].triggerEff
+                elif trees[len(trees)-1].category == '2M':
+                    if passCut(trees[len(trees)-1], 'tightSS'):
+                        if isData:
+                            DATA_SS_TT_cat2_veto += 1
+                        else:
+                            MC_SS_TT_cat2_veto += xs*(lumi/initEvents)*trees[len(trees)-1].triggerEff
+                    elif passCut(trees[len(trees)-1], 'relaxedOS'):
+                        if isData:
+                            DATA_OS_LL_cat2_veto += 1
+                        else:
+                            MC_OS_LL_cat2_veto += xs*(lumi/initEvents)*trees[len(trees)-1].triggerEff
+                    elif passCut(trees[len(trees)-1], 'relaxedSS'):
+                        if isData:
+                            DATA_SS_LL_cat2_veto += 1
+                        else:
+                            MC_SS_LL_cat2_veto += xs*(lumi/initEvents)*trees[len(trees)-1].triggerEff
+
+        print ''
+
+    QCD_SS_LL_cat1 = DATA_SS_LL_cat1 - MC_SS_LL_cat1
+    QCD_SS_TT_cat1 = DATA_SS_TT_cat1 - MC_SS_TT_cat1
+    QCD_OS_LL_cat1 = DATA_OS_LL_cat1 - MC_OS_LL_cat1
+
+    QCD_SS_LL_cat2 = DATA_SS_LL_cat2 - MC_SS_LL_cat2
+    QCD_SS_TT_cat2 = DATA_SS_TT_cat2 - MC_SS_TT_cat2
+    QCD_OS_LL_cat2 = DATA_OS_LL_cat2 - MC_OS_LL_cat2
+
+
+    QCD_SS_LL_cat0 = QCD_SS_LL_cat1 + QCD_SS_LL_cat2
+    QCD_SS_TT_cat0 = QCD_SS_TT_cat1 + QCD_SS_TT_cat2
+    QCD_OS_LL_cat0 = QCD_OS_LL_cat1 + QCD_OS_LL_cat2
+    DATA_OS_LL_cat0 = DATA_OS_LL_cat1 + DATA_OS_LL_cat2
+
+    SF_cat0 = QCD_SS_TT_cat0/QCD_SS_LL_cat0
+    sf_cat0 = QCD_OS_LL_cat0/DATA_OS_LL_cat0
+    weight0 = SF_cat0*sf_cat0
+
+    SF_cat1 = QCD_SS_TT_cat1/QCD_SS_LL_cat1
+    sf_cat1 = QCD_OS_LL_cat1/DATA_OS_LL_cat1
+    weight1 = SF_cat1*sf_cat1
+    
+    SF_cat2 = QCD_SS_TT_cat2/QCD_SS_LL_cat2
+    sf_cat2 = QCD_OS_LL_cat2/DATA_OS_LL_cat2
+    weight2 = SF_cat2*sf_cat2
+
+
+    QCD_SS_LL_cat1_veto = DATA_SS_LL_cat1_veto - MC_SS_LL_cat1_veto
+    QCD_SS_TT_cat1_veto = DATA_SS_TT_cat1_veto - MC_SS_TT_cat1_veto
+    QCD_OS_LL_cat1_veto = DATA_OS_LL_cat1_veto - MC_OS_LL_cat1_veto
+
+    QCD_SS_LL_cat2_veto = DATA_SS_LL_cat2_veto - MC_SS_LL_cat2_veto
+    QCD_SS_TT_cat2_veto = DATA_SS_TT_cat2_veto - MC_SS_TT_cat2_veto
+    QCD_OS_LL_cat2_veto = DATA_OS_LL_cat2_veto - MC_OS_LL_cat2_veto
+
+
+    QCD_SS_LL_cat0_veto = QCD_SS_LL_cat1_veto + QCD_SS_LL_cat2_veto
+    QCD_SS_TT_cat0_veto = QCD_SS_TT_cat1_veto + QCD_SS_TT_cat2_veto
+    QCD_OS_LL_cat0_veto = QCD_OS_LL_cat1_veto + QCD_OS_LL_cat2_veto
+    DATA_OS_LL_cat0_veto = DATA_OS_LL_cat1_veto + DATA_OS_LL_cat2_veto
+
+    SF_cat0_veto = QCD_SS_TT_cat0_veto/QCD_SS_LL_cat0_veto
+    sf_cat0_veto = QCD_OS_LL_cat0_veto/DATA_OS_LL_cat0_veto
+    weight0_veto = SF_cat0_veto*sf_cat0_veto
+
+    SF_cat1_veto = QCD_SS_TT_cat1_veto/QCD_SS_LL_cat1_veto
+    sf_cat1_veto = QCD_OS_LL_cat1_veto/DATA_OS_LL_cat1_veto
+    weight1_veto = SF_cat1_veto*sf_cat1_veto
+    
+    SF_cat2_veto = QCD_SS_TT_cat2_veto/QCD_SS_LL_cat2_veto
+    sf_cat2_veto = QCD_OS_LL_cat2_veto/DATA_OS_LL_cat2_veto
+    weight2_veto = SF_cat2_veto*sf_cat2_veto
+
+    if verbose:
+        print 'Results__________________________________'
+        print 'Category\t0\t\t1\t\t2\t\t0_veto\t\t1_veto\t\t2_veto'
+        print 'SF\t\t%.3f\t\t%.3f\t\t%.3f\t\t%.3f\t\t%.3f\t\t%.3f' %(SF_cat0, SF_cat1, SF_cat2, SF_cat0_veto, SF_cat1_veto, SF_cat2_veto)
+        print 'OCD/Data\t%.3f\t\t%.3f\t\t%.3f\t\t%.3f\t\t%.3f\t\t%.3f' %(sf_cat0, sf_cat1, sf_cat2, sf_cat0_veto, sf_cat1_veto, sf_cat2_veto)
+        print 'Weights\t\t%.3f\t\t%.3f\t\t%.3f\t\t%.3f\t\t%.3f\t\t%.3f' %(weight0, weight1, weight2, weight0_veto, weight1_veto, weight2_veto)
+
+    output = []
+    if 'veto' in out:
+        if '0' in out:
+            output.append(weight0_veto) 
+        if '1' in out:
+            output.append(weight1_veto) 
+        if '2' in out:
+            output.append(weight2_veto)
+    else:
+        if '0' in out:
+            output.append(weight0) 
+        if '1' in out:
+            output.append(weight1) 
+        if '2' in out:
+            output.append(weight2) 
+    return output
+
+# calculateSF(makeWholeSample_cfg.sampleConfigs, makeWholeSample_cfg.preFixTools, 'veto012',True)
