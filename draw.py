1e74bf60eeb761eeeaccc6980847cfd8eae87137
Zaixing <zaixing.mao@cern.ch>
2014-11-25 08:44:41 -0600

    config changes

--- draw.py
+++ draw.py
@@ -11,16 +11,15 @@ import makeWholeSample_cfg
 # import drawVarsData_new2
 
 #getHistos(varName, signalSelection, logY, sigBoost, nbins, useData, max, rangeMin, rangeMax, location):
-region = 'tight'
+region = 'semiTight'
 
 relPath = __file__
 script = os.path.abspath(relPath).replace(relPath, "%s" % draw_cfg.drawConfigs['script'])
 
-bTags = ['1M']
+bTags = ['1M1NonM']
 
 
-weights = makeWholeTools.calculateSF(makeWholeSample_cfg.sampleConfigsTools, makeWholeSample_cfg.preFixTools, 'SFveto012', region,True, 1.5)
-# weights = [0.0511, 0.0511, 0.0511]
+weights = makeWholeTools.calculateSF(makeWholeSample_cfg.sampleConfigsTools, makeWholeSample_cfg.preFixTools, 'SFveto012None', region,True)
 
 for ibTag in bTags:
     bTag = ibTag
@@ -30,6 +29,8 @@ for ibTag in bTags:
         weight = weights[1]
     elif bTag == '2M':
         weight = weights[2]
+    elif bTag == 'None':
+        weight = weights[3]
 
     for varName, varConfig in draw_cfg.varsRange.items():
 #         if ('pt2' in varName) or ('Pt' in varName):
@@ -42,7 +43,7 @@ for ibTag in bTags:
 #             yMax = 200
 #         else:
 #             yMax = 50
-        yMax = 100
+        yMax = 2000
         output = 'python %s --location %s  --signal %s' %(script, draw_cfg.drawConfigs['sampleLocation'], draw_cfg.drawConfigs['signal'])
         output += ' --variable %s --nbins %i --setRangeMin %f --setRangeMax %f' %(varName, varConfig[0], varConfig[1], varConfig[2])
         output += ' --setMax %i' %varConfig[3]
