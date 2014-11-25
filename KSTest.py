8ae02d977328231fe9e946e6df7bae45107bee9b
Zaixing <zaixing.mao@cern.ch>
2014-11-25 07:49:45 -0600

    ability to change signal regions

--- KSTest.py
+++ KSTest.py
@@ -6,73 +6,102 @@ import tool
 
 r.gROOT.SetBatch(True)  # to suppress canvas pop-outs
 
-def opts():
-    parser = optparse.OptionParser()
-    parser.add_option("-f", dest="file", default="", help="")
-    options, args = parser.parse_args()
-    return options
 
-options = opts()
+def KSTest(ifile, ofile, name):
 
-f = r.TFile(options.file)
-testTree = f.Get("TestTree")
-trainTree = f.Get("TrainTree")
+    f = r.TFile(ifile)
+    testTree = f.Get("TestTree")
+    trainTree = f.Get("TrainTree")
 
-nBins = 20
-xMin = -1.0
-xMax = 1.0
+    nBins = 20
+    nBins2 = 100000
+    xMin = -1.0
+    xMax = 1.0
 
-BDT_Sig_Train = r.TH1F('BDT_Sig_Train', 'BDT_Sig_Train', nBins, xMin, xMax)
-BDT_Sig_Test = r.TH1F('BDT_Sig_Test', 'BDT_Sig_Test', nBins, xMin, xMax)
-BDT_Bkg_Train = r.TH1F('BDT_Bkg_Train', 'BDT_Bkg_Train', nBins, xMin, xMax)
-BDT_Bkg_Test = r.TH1F('BDT_Bkg_Test', 'BDT_Bkg_Test', nBins, xMin, xMax)
+    BDT_Sig_Train = r.TH1F('BDT_Sig_Train', 'BDT_Sig_Train', nBins, xMin, xMax)
+    BDT_Sig_Test = r.TH1F('BDT_Sig_Test', 'Overtraining Check (%s)' %name, nBins, xMin, xMax)
+    BDT_Bkg_Train = r.TH1F('BDT_Bkg_Train', 'BDT_Bkg_Train', nBins, xMin, xMax)
+    BDT_Bkg_Test = r.TH1F('BDT_Bkg_Test', 'BDT_Bkg_Test', nBins, xMin, xMax)
 
-totalTest = testTree.GetEntries()
-for i in range(totalTest):
-    testTree.GetEntry(i)    
-    if testTree.className == "Signal":
-        BDT_Sig_Test.Fill(testTree.BDT, testTree.weight)
-    else:
-        BDT_Bkg_Test.Fill(testTree.BDT, testTree.weight)
+    BDT_Sig_Train_4KS = r.TH1F('BDT_Sig_Train_4KS', 'BDT_Sig_Train_4KS', nBins2, xMin, xMax)
+    BDT_Sig_Test_4KS = r.TH1F('BDT_Sig_Test_4KS', 'BDT_Sig_Test_4KS', nBins2, xMin, xMax)
+    BDT_Bkg_Train_4KS = r.TH1F('BDT_Bkg_Train_4KS', 'BDT_Bkg_Train_4KS', nBins2, xMin, xMax)
+    BDT_Bkg_Test_4KS = r.TH1F('BDT_Bkg_Test_4KS', 'BDT_Bkg_Test_4KS', nBins2, xMin, xMax)
 
-totalTrain = trainTree.GetEntries()
-for i in range(totalTrain):
-    trainTree.GetEntry(i)    
-    if trainTree.className == "Signal":
-        BDT_Sig_Train.Fill(trainTree.BDT, trainTree.weight)
-    else:
-        BDT_Bkg_Train.Fill(trainTree.BDT, trainTree.weight)
+    totalTest = testTree.GetEntries()
+    for i in range(totalTest):
+        testTree.GetEntry(i)    
+        if testTree.className == "Signal":
+            BDT_Sig_Test.Fill(testTree.BDT, testTree.weight)
+            BDT_Sig_Test_4KS.Fill(testTree.BDT, testTree.weight)
+        else:
+            BDT_Bkg_Test.Fill(testTree.BDT, testTree.weight)
+            BDT_Bkg_Test_4KS.Fill(testTree.BDT, testTree.weight)
 
-BDT_Bkg_Train.Sumw2()
-BDT_Sig_Train.Sumw2()
+    totalTrain = trainTree.GetEntries()
+    for i in range(totalTrain):
+        trainTree.GetEntry(i)    
+        if trainTree.className == "Signal":
+            BDT_Sig_Train.Fill(trainTree.BDT, trainTree.weight)
+            BDT_Sig_Train_4KS.Fill(trainTree.BDT, trainTree.weight)
+        else:
+            BDT_Bkg_Train.Fill(trainTree.BDT, trainTree.weight)
+            BDT_Bkg_Train_4KS.Fill(trainTree.BDT, trainTree.weight)
 
-print 'signal: %.4f' %(BDT_Sig_Test.KolmogorovTest(BDT_Sig_Train))
-print 'background: %.4f' %(BDT_Bkg_Test.KolmogorovTest(BDT_Bkg_Train))
+    BDT_Bkg_Train.Sumw2()
+    BDT_Sig_Train.Sumw2()
+    sigKS = BDT_Sig_Test_4KS.KolmogorovTest(BDT_Sig_Train_4KS)
+    bkgKS = BDT_Bkg_Test_4KS.KolmogorovTest(BDT_Bkg_Train_4KS)
+    print 'signal: %.4f' %sigKS
+    print 'background: %.4f' %bkgKS
 
-BDT_Bkg_Train.Scale(1/BDT_Bkg_Train.Integral())
-BDT_Bkg_Train.SetMarkerColor(r.kRed)
-BDT_Bkg_Train.SetMarkerStyle(21)
+    BDT_Bkg_Train.Scale(1/BDT_Bkg_Train.Integral())
+    BDT_Bkg_Train.SetMarkerColor(r.kRed)
+    BDT_Bkg_Train.SetMarkerStyle(21)
 
-BDT_Bkg_Test.SetLineColor(r.kRed)
-BDT_Bkg_Test.SetFillColor(r.kRed)
-BDT_Bkg_Test.SetFillStyle(3354)
-BDT_Bkg_Test.Scale(1/BDT_Bkg_Test.Integral())
+    BDT_Bkg_Test.SetLineColor(r.kRed)
+    BDT_Bkg_Test.SetFillColor(r.kRed)
+    BDT_Bkg_Test.SetFillStyle(3354)
+    BDT_Bkg_Test.Scale(1/BDT_Bkg_Test.Integral())
 
-BDT_Sig_Train.Scale(1/BDT_Sig_Train.Integral())
-BDT_Sig_Train.SetMarkerColor(r.kBlue)
-BDT_Sig_Train.SetMarkerStyle(21)
+    BDT_Sig_Train.Scale(1/BDT_Sig_Train.Integral())
+    BDT_Sig_Train.SetMarkerColor(r.kBlue)
+    BDT_Sig_Train.SetMarkerStyle(21)
 
-BDT_Sig_Test.SetLineColor(r.kBlue)
-BDT_Sig_Test.SetFillColor(r.kBlue)
-BDT_Sig_Test.SetFillStyle(3001) 
-BDT_Sig_Test.Scale(1/BDT_Sig_Test.Integral())
+    BDT_Sig_Test.SetLineColor(r.kBlue)
+    BDT_Sig_Test.SetFillColor(r.kBlue)
+    BDT_Sig_Test.SetFillStyle(3001) 
+    BDT_Sig_Test.Scale(1/BDT_Sig_Test.Integral())
+
+    legendHistos1 = []
+    legendHistos1.append((BDT_Bkg_Test, 'bkg test'))
+    legendHistos1.append((BDT_Bkg_Train, 'bkg train'))
+    legendHistos1.append((BDT_Bkg_Train, 'KS: %0.3f' %bkgKS))
+
+    legendHistos2 = []
+    legendHistos2.append((BDT_Sig_Test, 'sig test'))
+    legendHistos2.append((BDT_Sig_Train, 'sig train'))
+    legendHistos2.append((BDT_Sig_Train, 'KS: %0.3f' %sigKS))
 
 
+    l1 = tool.setMyLegend(lPosition=(0.2, 0.67, 0.5, 0.82), lHistList=legendHistos1)
+    l2 = tool.setMyLegend(lPosition=(0.6, 0.67, 0.9, 0.82), lHistList=legendHistos2)
 
-c = r.TCanvas("c","Test", 800, 600)
-BDT_Sig_Test.Draw()
-BDT_Sig_Test.SetMaximum(0.5)
-BDT_Sig_Train.Draw('sameE1P')
-BDT_Bkg_Test.Draw('same')
-BDT_Bkg_Train.Draw('sameE1P')
-c.Print('KS.pdf')
+    r.gStyle.SetOptStat(0)
+    c = r.TCanvas("c","Test", 800, 600)
+    BDT_Sig_Test.Draw()
+    BDT_Sig_Test.SetMaximum(0.5)
+    BDT_Sig_Train.Draw('sameE1P')
+    BDT_Bkg_Test.Draw('same')
+    BDT_Bkg_Train.Draw('sameE1P')
+    l1.Draw('same')
+    l2.Draw('same')
+
+    c.Print('%s.pdf' %ofile)
+
+massPoints = ['260','270','280','290','300','310','320','330','340','350']
+nTreesList = ['160', '170', '180', '190', '200']#['50', '60', '70', '80', '90', '100', '110', '120', '130', '140', '150']
+for nTrees in nTreesList:
+    for iMass in massPoints:
+        postFix = '_8_n%s_1M' %nTrees
+        KSTest('/scratch/zmao/TMVA/new3/TMVA%s%s.root' %(iMass,postFix), '/scratch/zmao/TMVA/pdf/TMVA%s%s' %(iMass,postFix), 'H2hh%s_n%s' %(iMass, nTrees))
