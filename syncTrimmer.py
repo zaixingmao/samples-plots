3bfe2ef5aea2a05bec2f7c04915857e86cae3873
Zaixing <zaixing.mao@cern.ch>
2014-12-09 15:20:34 -0600

    fix

--- syncTrimmer.py
+++ syncTrimmer.py
@@ -20,32 +20,14 @@ def opts():
     return options
 
 def passCut(iTree, cut):
-    if 'none' in cut:
+    runRanges = {'A': (0,193621),
+                'B': (193621,196531),
+                'C': (196531,203742),
+                'D': (203742,208686)}
+    if runRanges[cut][0] < iTree.run <= runRanges[cut][1]:
         return True
-    if 'tight' in cut:
-        if iTree.byCombinedIsolationDeltaBetaCorrRaw3Hits_1 > 1 or iTree.byCombinedIsolationDeltaBetaCorrRaw3Hits_2 > 1:
-            return False
-    if 'same' in cut:
-        if iTree.q_1 == -iTree.q_2:
-            return False
-    if 'opposite' in cut:
-        if iTree.q_1 == iTree.q_2:
-            return False
-    if '1M' in cut:
-        if iTree.bcsv_2 <= 0:
-            return False
-        if iTree.bpt_1 < 0.679 or iTree.bcsv_2 > 0.679:
-            return False
-        if abs(iTree.beta_1) > 2.4 or abs(iTree.beta_2) > 2.4:
-            return False
-    if '2M' in cut:
-        if iTree.bcsv_2 <= 0:
-            return False
-        if iTree.bpt_1 < 0.679 or iTree.bcsv_2 < 0.679:
-            return False
-        if abs(iTree.beta_1) > 2.4 or abs(iTree.beta_2) > 2.4:
-            return False
-    return True
+    else:
+        return False
 
 options = opts()
 iFile = options.inputFile
@@ -59,21 +41,13 @@ for i in range(total):
     r.gStyle.SetOptStat(0)
     tool.printProcessStatus(iCurrent=i, total=total, processName = 'Looping sample %s' %iFile)
     iTree.GetEntry(i)
-    if iTree.bpt_1 < 20:
-        continue
-    if iTree.bpt_2 < 20:
-        continue
     if passCut(iTree, options.cut):
         oTree.Fill()
-        sigYeild += iTree.trigweight_1*iTree.trigweight_2
 print ''
 
-# print (sigYeild/457250.0)*19.7*1000
-print (sigYeild/570356.0)*19.7*1000
+oFile.cd()
+oTree.Write()
+nSaved = oTree.GetEntries()
+oFile.Close()
 
-# oFile.cd()
-# oTree.Write()
-# nSaved = oTree.GetEntries()
-# oFile.Close()
-# 
-# print 'saved %i events out of %i at: %s_%s.root' %(nSaved,total,options.inputFile, options.cut)
+print 'saved %i events out of %i at: %s_%s.root' %(nSaved,total,options.inputFile, options.cut)
