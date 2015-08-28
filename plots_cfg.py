33e953436c2f57d9dd84c9f1962c4f5107f86937
Zaixing <zaixing.mao@cern.ch>
2015-08-28 10:22:19 -0500

    config changes

--- plots_cfg.py
+++ plots_cfg.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
 from array import array
 
-dir = "/nfs_scratch/zmao/13TeV_samples_25ns/"
+dir = "/nfs_scratch/zmao/13TeV_samples_25ns_fixSelectionOrder"
 
 sampleList = [
-                ('%s/WJets_all_SYNC_' %dir, 'Electroweak', 24089991),
+                ('%s_2/WJets_all_SYNC_' %dir, 'Electroweak', 24089991),
               ('%s/WW_all_SYNC_' %dir, 'Electroweak', 989608),
               ('%s/WZ_all_SYNC_' %dir, 'Electroweak', 996920),
               ('%s/ZZ_all_SYNC_' %dir, 'Electroweak', 998848),
-              ('%s/DY_all_SYNC_' %dir, 'Z#rightarrow#tau#tau', 19925500),
+              ('%s_2/DY_all_SYNC_' %dir, 'Z#rightarrow#tau#tau', 19925500),
               ('%s/antiT_all_SYNC_' %dir, 't#bar{t}', 500000),
               ('%s/T_all_SYNC_' %dir, 't#bar{t}', 998400),
-              ('%s/TTJets_all_SYNC_' %dir, 't#bar{t}', 4994250),
-              ('%s/data_all_SYNC_' %dir, 'Observed', 1),
+              ('%s_2/TTJets_all_SYNC_' %dir, 't#bar{t}', 4994250),
+              ('/nfs_scratch/zmao/13TeV_samples/data_all_SYNC_', 'Observed', 1),
             ]
 
 
@@ -20,14 +20,18 @@ dataCardSamplesList = [('WJets', '%s/WJets_all_SYNC' %dir, 0),#24089991),
               ('WW','%s/WW_all_SYNC' %dir, 0),#989608),
               ('WZ', '%s/WZ_all_SYNC' %dir, 0),#996920),
               ('ZZ', '%s/ZZ_all_SYNC' %dir, 0),#998848),
-              ('DY-50', '%s/DY_all_SYNC' %dir, 0),#19925500),
-              ('DY-10to50', '%s/DY-10to50_all_SYNC' %dir, 0),#),
+              ('ZLL', '/nfs_scratch/zmao/13TeV_samples_25ns_2/DY_ZLL_all_SYNC', 0),#19925500),
+              ('ZTT', '/nfs_scratch/zmao/13TeV_samples_25ns_2/DY_ZTT_all_SYNC', 0),#19925500),
+              ('ZJ', '/nfs_scratch/zmao/13TeV_samples_25ns_2/DY_ZJ_all_SYNC', 0),#19925500),
+#               ('DY-50', '%s_2/DY_all_SYNC' %dir, 0),#19925500),
+#               ('DY-10to50', '%s_2/DY-10to50_all_SYNC' %dir, 0),#),
               ('ST_antiTop_tW', '%s/antiT_all_SYNC' %dir, 0),#500000),
               ('ST_top_tW', '%s/T_all_SYNC' %dir, 0),#998400),
-              ('ST_antiTop_t-channel', '%s/antiT-tchannel_all_SYNC' %dir, 0),#),
-              ('ST_top_t-channel', '%s/antiT_all_SYNC' %dir, 0),#),
+#               ('ST_antiTop_t-channel', '%s/antiT-tchannel_all_SYNC' %dir, 0),#),
+#               ('ST_top_t-channel', '%s/antiT_all_SYNC' %dir, 0),#),
               ('TTJets', '%s/TTJets_all_SYNC' %dir, 0),#4994250),
-              ('data', '/nfs_scratch/zmao/13TeV_samples/data_all_SYNC', 0),
+              ('ggH160', '%s/SUSY_all_SYNC' %dir, 0),
+              ('data', '/nfs_scratch/zmao/13TeV_samples_50ns/data_all_SYNC', 0),
             ]
 
 # selection = '_antiIso'
