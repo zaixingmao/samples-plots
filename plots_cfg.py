c42340f02b254c4a5dc5f1ab95fac750a274398b
Edward Laird <elaird@gmail.com>
2015-09-18 14:46:27 -0400

    fix typos

--- plots_cfg.py
+++ plots_cfg.py
@@ -15,8 +15,8 @@ sampleList = [
     ('ST_top_tW', '%s/T_all_SYNC_' %dir, 't#bar{t}'),
     ('TTJets', '%s/TTJets_all_SYNC_' %dir, 't#bar{t}'),
     ('data', '%s/data_all_SYNC_' %dir, 'Observed'),
-    ('ggH160', '%s/SUSY_all_SYNC' %dir, 'ggH160'),
-    ] + [('Zprime%d' % m, '%s/ZPrime_%d_all_SYNC' % (dir, m), 'ZPrime_%d' % m) for m in (set(range(500, 5500, 500)) - set([2500, 4500]))]
+    ('ggH160', '%s/SUSY_all_SYNC_' %dir, 'ggH160'),
+    ] + [('Zprime%d' % m, '%s/ZPrime_%d_all_SYNC_' % (dir, m), 'ZPrime_%d' % m) for m in (set(range(500, 5500, 500)) - set([2500, 4500]))]
 
 
 # selection = '_antiIso'
