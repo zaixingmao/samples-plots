22556915b172616790090ce278e1b02d6f0275f0
zaixingmao <zaixing.mao@cern.ch>
2015-10-14 16:18:23 -0400

    config changes

--- plots_cfg.py
+++ plots_cfg.py
@@ -2,21 +2,26 @@
 from array import array
 
 dir = "/nfs_scratch/zmao/13TeV_samples_25ns_Spring15_eletronID2"
-
+dir = "/user_data/elaird/svSkim-sep18/"
+dir_data = "/user_data/zmao/RunD_svfit/"
+dir = "/user_data/zmao/noIso/"
+dir_data = "/user_data/zmao/RunD_594/"
 sampleList = [
-    ('WJets', '%s/WJets_all_SYNC_' %dir, 'Electroweak'),
+    ('WJets', '%s/WJets_all_SYNC_' %dir, 'WJets'),
     ('WW', '%s/WW_all_SYNC_' %dir, 'Electroweak'),
     ('WZ', '%s/WZ_all_SYNC_' %dir, 'Electroweak'),
     ('ZZ', '%s/ZZ_all_SYNC_' %dir, 'Electroweak'),
-    ('ZTT', '%s/DY_all_ZTT_SYNC_' %dir, 'Z#rightarrow#tau#tau'),
-    ('ZL', '%s/DY_all_ZL_SYNC_' %dir, 'Z#rightarrow#tau#tau'),
-    ('ZJ', '%s/DY_all_ZJ_SYNC_' %dir, 'Z#rightarrow#tau#tau'),
+    ('ZTT', '%s/DY_all_SYNC_' %dir, 'Z#rightarrow#tau#tau'),
+#     ('ZTT', '%s/DY_all_ZTT_SYNC_' %dir, 'Z#rightarrow#tau#tau'),
+#     ('ZL', '%s/DY_all_ZL_SYNC_' %dir, 'Z#rightarrow#tau#tau'),
+#     ('ZJ', '%s/DY_all_ZJ_SYNC_' %dir, 'Z#rightarrow#tau#tau'),
     ('ST_antiTop_tW', '%s/antiT_all_SYNC_' %dir, 't#bar{t}'),
     ('ST_top_tW', '%s/T_all_SYNC_' %dir, 't#bar{t}'),
     ('TTJets', '%s/TTJets_all_SYNC_' %dir, 't#bar{t}'),
     ('data', '%s/data_all_SYNC_' %dir, 'Observed'),
-    ('ggH160', '%s/SUSY_all_SYNC_' %dir, 'ggH160'),
-    ] + [('Zprime%d' % m, '%s/ZPrime_%d_all_SYNC_' % (dir, m), 'ZPrime_%d' % m) for m in (set(range(500, 5500, 500)) - set([2500, 4500]))]
+#    ('ggH160', '%s/SUSY_all_SYNC_' %dir, 'ggH160'),
+    ]# + [('Zprime_2000', '%s/ZPrime_2000_all_SYNC_' %dir, 'ZPrime_2000')]
+    #]+ [('Zprime%d' % m, '%s/ZPrime_%d_all_SYNC_' % (dir, m), 'ZPrime_%d' % m) for m in (set(range(500, 5500, 500)) - set([2500, 4500]))]
 
 
 # selection = '_antiIso'
@@ -37,27 +42,54 @@ nBins = 10
 bins = []
 for i in range(nBins+1):
     bins.append(-3.0 + (6*i+0.0)/nBins)
+bins2 = []
+nBins = 20
+for i in range(nBins+1):
+    bins2.append(-1.0 + (i+0.0)/nBins)
+bins3 = []
+nBins = 40
+for i in range(nBins+1):
+    bins3.append(0.0 + (2*i+0.0)/nBins)
+vars = [# ("ePt", array('d', range(0,120,15)), 'GeV', 0.25),
+#         ("mPt", array('d', range(0,120,15)), 'GeV', 0.25),
+#       ("tPt", array('d', range(0, 200, 10)), 'GeV', 0.25),
+#        ("eEta", array('d', bins), 'GeV', 80), 
+#        ("mEta", array('d', bins), 'GeV', 80),
+#        ("m_vis", array('d', range(400, 2600, 200)), 'GeV', 0.25),
+#        ("pfmet_svmc_mass", array('d', range(400, 2600, 200)), 'GeV', 0.25),
+#        ("m_withMET", array('d', range(400, 2600, 200)), 'GeV', 0.25),
 
-vars = [# ("pt_1", array('d', range(0,200,10)), 'GeV', 0.25),
-#         ("pt_2", array('d', range(0, 200, 10)), 'GeV', 0.25),
-#         ("eta_1", array('d', bins), 'GeV', 80),
-#         ("eta_2", array('d', bins), 'GeV', 80),
+#        ("pfmet_svmc_mass", array('d', range(0, 200, 20) + range(200, 400, 50) + range(400, 2600, 200)), 'GeV', 0.25),
+#        ("m_vis", array('d', range(0, 200, 20) + range(200, 400, 50) + range(400, 2600, 200)), 'GeV', 0.25),
+#        ("m_withMET", array('d', range(0, 200, 20) + range(200, 400, 50) + range(400, 2600, 200)), 'GeV', 0.25),
+#         ("m_vis", array('d', range(0, 200, 15)), 'GeV', 0.25),
+#        ("pfmet_svmc_mass", array('d', range(0, 300, 10)), 'GeV', 0.25),
+#        ("m_withMET", array('d', range(0, 5005, 5)), 'GeV', 0.25),
+#        ("pfmet_svmc_mass", array('d', range(0, 5005, 5)), 'GeV', 0.25),
+#         ("m_vis", array('d', range(0, 5005, 5)), 'GeV', 0.25),
 
-        ("m_vis", array('d', range(0, 200, 20) + range(200, 400, 50) + range(400, 2600, 200)), 'GeV', 0.25),
-       ("m_withMET", array('d', range(0, 200, 20) + range(200, 400, 50) + range(400, 2600, 200)), 'GeV', 0.25),
-        ("m_gen", array('d', range(0, 200, 20) + range(200, 400, 50) + range(400, 2600, 200)), 'GeV', 0.25),
+#        ("pZeta - 3.1pZetaVis", array('d', range(-200, 100, 10)), '', 0.25),
+#        ("pfMetEt", array('d', range(0, 300, 10)), '', 0.25),
+#        ("nCSVL", array('d', range(0, 10, 1)), '', 0.25),
+#        ("cos_phi_tau1_tau2", array('d', bins2), '', 0.25),
+       ("mRelIso", array('d', bins3), '', 0.25),
+
+#        ("pfmet_svmc_mass", array('d', range(0, 200, 20) + range(200, 400, 50) + range(400, 2600, 200)), 'GeV', 0.25),
+
+#        ("m_withMET", array('d', range(0, 200, 20) + range(200, 400, 50) + range(400, 2600, 200)), 'GeV', 0.25),
+#        ("m_gen", array('d', range(0, 200, 20) + range(200, 400, 50) + range(400, 2600, 200)), 'GeV', 0.25),
 
 #         ("mt_1", array('d', range(0, 200, 10)), 'GeV', 12),
 #         ("mt_2", array('d', range(0, 200, 10)), 'GeV', 12),
-#         ("npv", array('d', range(0, 35, 1)), '', 0.15),
+#         ("nvtx", array('d', range(0, 35, 1)), '', 0.15),
 
 #         ("njets", array('d', range(0,6,1)), '', 100),
 #         ("met", array('d', range(0, 200, 10)), 'GeV', 80),
         ]
 
-QCD_scale = {'tt': 1.073,
-              'et': 0.981,
-              'mt': 1.012,
+QCD_scale = {'tt': 1.0,
+              'et': 1.09,
+              'mt': 1.0,
               'em': 1.0}
 
 addIntegrals = True
