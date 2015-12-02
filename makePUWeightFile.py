77c699135ae10814ee764b40d85fd7cf99899f68
zaixingmao <zaixing.mao@cern.ch>
2015-12-01 20:19:39 -0500

    update puweight

--- makePUWeightFile.py
+++ makePUWeightFile.py
@@ -47,6 +47,67 @@ dataLocations = {'em': '/hdfs/store/user/zmao/data_25ns_Spring15_eletronID/data_
                 'tt':'/hdfs/store/user/zmao/data_25ns_Spring15_eletronID/data_Tau_Run2015C_PromptReco_25ns/'
                 }
 
+def make_MC_PUDistribution():
+    dist = [4.8551E-07,
+            1.74806E-06,
+            3.30868E-06,
+            1.62972E-05,
+            4.95667E-05,
+            0.000606966,
+            0.003307249,
+            0.010340741,
+            0.022852296,
+            0.041948781,
+            0.058609363,
+            0.067475755,
+            0.072817826,
+            0.075931405,
+            0.076782504,
+            0.076202319,
+            0.074502547,
+            0.072355135,
+            0.069642102,
+            0.064920999,
+            0.05725576,
+            0.047289348,
+            0.036528446,
+            0.026376131,
+            0.017806872,
+            0.011249422,
+            0.006643385,
+            0.003662904,
+            0.001899681,
+            0.00095614,
+            0.00050028,
+            0.000297353,
+            0.000208717,
+            0.000165856,
+            0.000139974,
+            0.000120481,
+            0.000103826,
+            8.88868E-05,
+            7.53323E-05,
+            6.30863E-05,
+            5.21356E-05,
+            4.24754E-05,
+            3.40876E-05,
+            2.69282E-05,
+            2.09267E-05,
+            1.5989E-05,
+            4.8551E-06,
+            2.42755E-06,
+            4.8551E-07,
+            2.42755E-07,
+            1.21378E-07,
+            4.8551E-08]
+    pileup = r.TH1F('pileup', '', 50, 0, 50)
+    outputFileName = "%s/MC_50bins.root" %(options.location)
+    oFile = r.TFile(outputFileName,"recreate")
+    for i in range(len(dist)):
+        pileup.Fill(i, dist[i])
+    oFile.cd()
+    pileup.Write()
+    oFile.Close()
 
 def loop_one_sample(iSample, iLocation):
     print 'making sample [%s]' %(iSample)
@@ -56,7 +117,7 @@ def loop_one_sample(iSample, iLocation):
     else:
         isData = False
         
-    pileup = r.TH1F('pileup', '', 60, 0, 60)
+    pileup = r.TH1F('pileup', '', 50, 0, 50)
 
     if isData:
         outputFileName = "%s/data_600bins.root" %(options.location)
@@ -103,4 +164,5 @@ def go():
 
 
 if __name__ == "__main__":
-    go()
+#     go()
+    make_MC_PUDistribution()
