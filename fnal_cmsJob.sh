67552b547729125840c8a1bd8bd9940039b8f8b4
zaixingmao <zaixing.mao@cern.ch>
2016-05-19 13:59:03 -0500

    fix arch

--- fnal_cmsJob.sh
+++ fnal_cmsJob.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 source /cvmfs/cms.cern.ch/cmsset_default.sh
-export SCRAM_ARCH=slc6_amd64_gcc491
+export SCRAM_ARCH=slc6_amd64_gcc493
 
-cd /cvmfs/cms.cern.ch/slc6_amd64_gcc491/cms/cmssw/CMSSW_7_6_3/src && eval `scram runtime -sh` && cd - >& /dev/null
+cd /cvmfs/cms.cern.ch/slc6_amd64_gcc493/cms/cmssw/CMSSW_7_6_3/src && eval `scram runtime -sh` && cd - >& /dev/null
 
 #INSERT_BATCH_SETUP
 
